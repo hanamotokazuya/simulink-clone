dfd7d4e50793a3290f35abe80b5fb000feee7de0
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-06 13:44:10 +0900

    feat(Menu): Create Menu component

---
+++ components/Menu.tsx
@@ -0,0 +1,15 @@
+const Menu: React.FC = () => {
+  return (
+    <div className="h-full w-1/5 bg-gray-300 flex flex-col items-start">
+      <div>Component 1</div>
+      <div>Component 2</div>
+      <div>Component 3</div>
+      <div>Component 4</div>
+      <div>Component 5</div>
+      <div>Component 6</div>
+      <div>Component 7</div>
+    </div>
+  );
+};
+
+export default Menu;
