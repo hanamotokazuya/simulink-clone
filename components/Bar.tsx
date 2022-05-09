1f9f3bbb7727f67ba260c0e459ac8e00dc9a41ae
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-09 14:47:40 +0900

    feat(Bar): Implement Bar component

---
+++ components/Bar.tsx
@@ -0,0 +1,15 @@
+import { ComponentBase } from "../lib/component";
+
+const Bar: React.FC = () => {
+  const handleExec = () => {
+    ComponentBase.run();
+    console.log(ComponentBase.results);
+  };
+  return (
+    <div className="fixed top-0 left- 0 w-full h-10 bg-gray-200 flex items-center z-50">
+      <div className="bg-green-400 w-8 h-8 rounded-full" onClick={handleExec}></div>
+    </div>
+  );
+};
+
+export default Bar;
