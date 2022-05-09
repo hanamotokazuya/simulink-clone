99374cac886c92dc8ce86136264c37d2bab1ac65
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-09 14:44:52 +0900

    feat(StateContext): Added components state

--- context/StateContext.tsx
+++ context/StateContext.tsx
@@ -1,14 +1,29 @@
-import { useContext, useReducer, useState, createContext } from "react";
+import { useContext, useReducer, createContext } from "react";
+import _ from "lodash";
+import { ComponentBase, setComponent } from "../lib/component";
 
 type State = {
   selectComponent: string;
+  components: ComponentBase[];
 };
 
-type Action = SELECT_COMPONENT;
+type Action = SELECT_COMPONENT | SET_COMPONENT | CHANGE_PROPERTIES;
 
 type SELECT_COMPONENT = {
   type: "SELECT_COMPONENT";
   component: string;
+};
+type SET_COMPONENT = {
+  type: "SET_COMPONENT";
+  posX: number;
+  posY: number;
+};
+type CHANGE_PROPERTIES = {
+  type: "CHANGE_PROPERTIES";
+  id: number;
+  property: string[];
+  values: string[];
+  inport: string;
 };
 
 const StateContext = createContext({} as { state: State; action: React.Dispatch<Action> });
@@ -21,12 +36,19 @@ export const StateContextProvider: React.FC<Props> = ({ children }) => {
     switch (action.type) {
       case "SELECT_COMPONENT":
         return { ...state, selectComponent: action.component };
+      case "SET_COMPONENT":
+        if (state.selectComponent) {
+          setComponent(state.selectComponent, action.posX, action.posY);
+          return { ...state, selectComponent: "" };
+        }
+        return { ...state };
       default:
         return state;
     }
   };
   const initializeState: State = {
     selectComponent: "",
+    components: ComponentBase.components,
   };
   const [state, action] = useReducer(events, initializeState);
   return <StateContext.Provider value={{ state, action }}>{children}</StateContext.Provider>;
